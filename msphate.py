01737cba5e7d4c2d79044f0f8d3a4ac34804ba5c
arthuss <artemiyhussnain@gmail.com>
2022-12-13 14:51:22 +1030

    Big change to msphate.py, started making final report

--- msphate.py
+++ msphate.py
@@ -8,7 +8,6 @@ wdir = '~/Jansky/ms_phate/msphate_jansky/msPHATE-Seurat'
 npca = 20 # Deafault value for RunPCA in Seurat
 gran = 0.1
 vis_level = 0 # Always zero so points on embedding correspond to individual cells
-clus_level = 3
 
 wdir = os.path.expanduser(wdir)
 gene_path = os.path.join(wdir, 'gene_names.txt')
@@ -17,31 +16,31 @@ cell_path = os.path.join(wdir, 'cell_names.txt')
 print('Loading scaled data...')
 data_path = os.path.join(wdir, 'counts.csv')
 data = scprep.io.load_csv(data_path, cell_axis='column',
-                          gene_names=gene_path,
-                          cell_names=cell_path)
+                        gene_names=gene_path,
+                        cell_names=cell_path)
 
 mp_op = mp.Multiscale_PHATE(n_pca=npca, granularity=gran, random_state=0)
 levels = mp_op.fit(data)
 
-print('Generating embedding and assigning clusters...')
-embedding, clusters, sizes = mp_op.transform(visualization_level = levels[vis_level],
-                                                             cluster_level = levels[-1*clus_level])
+def msphate(clustering_level):
+    clus_level = clustering_level
+    print('Generating embedding and assigning clusters...')
+    embedding, clusters, sizes = mp_op.transform(visualization_level = levels[vis_level],
+                                                               cluster_level = levels[-1*clus_level])
 
-print('Exporting embedding and clusters...')
-msPHATE_embedding = pd.DataFrame(embedding,
-                        index = pd.read_csv(cell_path, header=None).iloc[:, 0].to_list())
-msPHATE_embedding.to_csv('msPHATE_embedding.csv', header=False)
-msPHATE_clusters = pd.DataFrame(clusters)
-msPHATE_clusters.to_csv('msPHATE_clusters.csv', header=False, index=False)
+    print('Exporting embedding and clusters...')
+    msPHATE_embedding = pd.DataFrame(embedding,
+                          index = pd.read_csv(cell_path, header=None).iloc[:, 0].to_list())
+    msPHATE_embedding.to_csv('msPHATE_embedding.csv', header=False)
+    msPHATE_clusters = pd.DataFrame(clusters)
+    msPHATE_clusters.to_csv('msPHATE_clusters.csv', header=False, index=False)
 
-print('\n')
-print(data)
-print('\n')
-print(levels)
-print('\n')
-print(embedding)
-print('\n')
-print(set(clusters))
-print('\n')
-print('clus_level '+str(clus_level))
-print('Done')
+    print('\n')
+    print(data)
+    print('\n')
+    print(levels)
+    print('\n')
+    print(embedding)
+    print('\n')
+    print(set(clusters))
+    print('Done')
